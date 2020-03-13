0e562b281815a613a1bde9d3972e6f3ad1d2a759
Vanilna <lesyalp@gmail.com>
2020-03-13 12:59:12 +0100

    add initial styling to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
+import background from "./assets/background.png";
+
+const AppWrapper = styled.div`
+  background-image: url(${background});
+  height: 100vh;
+  position: relative;
+`;
+
+const Credentials = styled.p`
+  color: #ffffff;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 0.5rem;
+  a {
+    color: #ffffff;
+    text-decoration: none;
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppWrapper>
+      <Credentials>
+        Image by{" "}
+        <a href="https://pixabay.com/users/WikiImages-1897/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=67522">
+          WikiImages
+        </a>{" "}
+        from{" "}
+        <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=67522">
+          Pixabay
+        </a>
+      </Credentials>
+    </AppWrapper>
+  );
 }
 
 export default App;

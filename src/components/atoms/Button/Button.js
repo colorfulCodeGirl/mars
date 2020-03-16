a3b9edba50aca8a78938fa2ef1b65133fa529b46
Vanilna <lesyalp@gmail.com>
2020-03-16 09:14:20 +0100

    styled button component

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -5,10 +5,17 @@ const StyledButton = styled.button`
   border: none;
   padding: 0.9rem 2.7rem;
   background-color: #fe6c3d;
+  color: #ffffff;
+  border-radius: 0.4rem;
+  &:disabled {
+    background-color: #515050;
+  }
 `;
 
 const Button = ({ children, clickHandler, ...props }) => (
-  <StyledButton onClick={clickHandler}>{children}</StyledButton>
+  <StyledButton onClick={clickHandler} {...props}>
+    {children}
+  </StyledButton>
 );
 
 export default Button;

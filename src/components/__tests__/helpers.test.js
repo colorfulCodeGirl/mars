40aee88317ee80948d27b5b27e668d89efb4d565
Vanilna <lesyalp@gmail.com>
2020-03-20 11:25:03 +0100

    add tests for formateDate function

--- src/components/__tests__/helpers.test.js
+++ src/components/__tests__/helpers.test.js
@@ -1,6 +1,6 @@
 import { validateDate, formateDate } from "../../helpers";
 
-describe("Validate date function", () => {
+describe("validateDate function", () => {
   const mockRange = {
     startDate: "2008-06-07",
     endDate: "2017-09-15"
@@ -18,5 +18,19 @@ describe("Validate date function", () => {
     expect(validateDate(mockRange, "2015-07-006")).toBeFalsy();
 
     expect(validateDate(mockRange, "2019-07-09")).toBeFalsy();
+  });
+});
+
+describe("formateDate function", () => {
+  it("formate date adding dashes", () => {
+    expect(formateDate("2015", "201")).toBe("2015-");
+    expect(formateDate("2015-08", "2015-0")).toBe("2015-08-");
+    expect(formateDate("2015-0", "2015-")).toBe("2015-0");
+    expect(formateDate("201", "20")).toBe("201");
+  });
+
+  it("allows to remove dashes", () => {
+    expect(formateDate("2015", "2015-")).toBe("2015");
+    expect(formateDate("2015-09", "2015-09-")).toBe("2015-09");
   });
 });

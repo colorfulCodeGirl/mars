468412df93718a739a62faa9269dd9e180c74ad3
Vanilna <lesyalp@gmail.com>
2020-03-23 12:24:22 +0100

    search form tests - add tests for rendering rover select and search button

--- src/components/__tests__/SearchForm.test.js
+++ src/components/__tests__/SearchForm.test.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getRoles } from "@testing-library/react";
 import SearchForm from "../organisms/SearchForm/SearchForm";
 
 describe("Search form component", () => {
   it("renders heading", () => {
     const { getByText } = render(<SearchForm />);
     expect(getByText(/explore/i)).toBeInTheDocument();
+  });
+
+  it("renders select element for rovers", () => {
+    const { getByLabelText } = render(<SearchForm />);
+    expect(getByLabelText(/choose/i)).toBeInTheDocument();
+  });
+  it("renders search button", () => {
+    const { getByText } = render(<SearchForm />);
+    const button = getByText(/search/i);
+    expect(button).toBeInTheDocument();
+    expect(getRoles(button).button).toBeTruthy();
+  });
+
+  it("renders button disabled on start", () => {
+    const { getByText } = render(<SearchForm />);
+    expect(getByText(/search/i)).toBeDisabled();
   });
 });

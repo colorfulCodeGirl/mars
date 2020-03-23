1480cd37385e2156994379b2352522d3c6c94c76
Vanilna <lesyalp@gmail.com>
2020-03-23 12:35:04 +0100

    search form tests - add tests for buttos to be disabled/enabled

--- src/components/__tests__/SearchForm.test.js
+++ src/components/__tests__/SearchForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, getRoles } from "@testing-library/react";
+import { render, getRoles, fireEvent } from "@testing-library/react";
 import SearchForm from "../organisms/SearchForm/SearchForm";
 
 describe("Search form component", () => {
@@ -18,9 +18,32 @@ describe("Search form component", () => {
     expect(button).toBeInTheDocument();
     expect(getRoles(button).button).toBeTruthy();
   });
+  it("renders 'see latest' button", () => {
+    const { getByText } = render(<SearchForm />);
+    const button = getByText(/see latest/i);
+    expect(button).toBeInTheDocument();
+    expect(getRoles(button).button).toBeTruthy();
+  });
 
-  it("renders button disabled on start", () => {
+  it("renders buttons disabled on start", () => {
     const { getByText } = render(<SearchForm />);
     expect(getByText(/search/i)).toBeDisabled();
+    expect(getByText(/see latest/i)).toBeDisabled();
+  });
+  it("enables see latest button when rover is picked", () => {
+    const { getByText, getByLabelText } = render(<SearchForm />);
+    const select = getByLabelText(/choose rover/i);
+    const button = getByText(/see latest/i);
+    expect(button).toBeDisabled();
+    fireEvent.change(select, { target: { value: "opportunity" } });
+    expect(button).toBeEnabled();
+  });
+  it("doesn't enables search button when only rover is picked", () => {
+    const { getByText, getByLabelText } = render(<SearchForm />);
+    const select = getByLabelText(/choose rover/i);
+    const button = getByText(/search/i);
+    expect(button).toBeDisabled();
+    fireEvent.change(select, { target: { value: "opportunity" } });
+    expect(button).toBeDisabled();
   });
 });

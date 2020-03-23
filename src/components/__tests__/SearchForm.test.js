6153d5a0c6749ee1b01a155cdc1fa35a037fb1ee
Vanilna <lesyalp@gmail.com>
2020-03-23 13:11:26 +0100

    search form tests - add tests for changing input for days when type is changed

--- src/components/__tests__/SearchForm.test.js
+++ src/components/__tests__/SearchForm.test.js
@@ -46,4 +46,37 @@ describe("Search form component", () => {
     fireEvent.change(select, { target: { value: "opportunity" } });
     expect(button).toBeDisabled();
   });
+  it("renders radio group component when rover is picked", () => {
+    const { getAllByRole, getByLabelText, getAllByLabelText } = render(
+      <SearchForm />
+    );
+    const select = getByLabelText(/choose rover/i);
+    fireEvent.change(select, { target: { value: "opportunity" } });
+    const radio = getAllByRole(/radio/i);
+    expect(radio.length).toBe(2);
+    const solRadio = getAllByLabelText(/sol/i);
+    const earthDayRadio = getByLabelText(/earth/i);
+    expect(solRadio[0]).toBeInTheDocument();
+    expect(earthDayRadio).toBeInTheDocument();
+  });
+  it("renders input element for sol/earth days when rover is picked", () => {
+    const { getByLabelText, getByPlaceholderText } = render(<SearchForm />);
+    const select = getByLabelText(/choose rover/i);
+    fireEvent.change(select, { target: { value: "opportunity" } });
+    const selectDay = getByPlaceholderText(/SOL from 0 to/i);
+    expect(selectDay).toBeInTheDocument();
+  });
+  it("changes date input element from sol to Earth days when radio group is changed", () => {
+    const { getByLabelText, getByPlaceholderText, getAllByRole } = render(
+      <SearchForm />
+    );
+    const select = getByLabelText(/choose rover/i);
+    fireEvent.change(select, { target: { value: "opportunity" } });
+    const radioBtns = getAllByRole(/radio/i);
+    fireEvent.click(radioBtns[1]);
+    expect(radioBtns[0].checked).toBeFalsy();
+    expect(radioBtns[1].checked).toBeTruthy();
+    const input = getByPlaceholderText(/Date from/i);
+    expect(input).toBeInTheDocument();
+  });
 });

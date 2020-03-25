6d81477edfb9c0ef81e1a01a1b9a342ddb607620
Vanilna <lesyalp@gmail.com>
2020-03-25 15:38:33 +0100

    add tests for App component

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "../../App";
+
+describe("App component", () => {
+  it("changes changes form display if photos are shown", () => {
+    const { getByTestId, getByLabelText, getByText } = render(<App />);
+
+    const form = getByTestId("form");
+    expect(form).toHaveStyle(`
+    justify-self: center;
+    align-self: center;
+    `);
+
+    const select = getByLabelText(/choose rover/i);
+    fireEvent.change(select, { target: { value: "curiosity" } });
+
+    // const inputSol = getByPlaceholderText(/SOL from 0 to/i);
+    // fireEvent.change(inputSol, { target: { value: "0" } });
+
+    const button = getByText(/see latest/i);
+
+    fireEvent.click(button);
+    expect(form).toHaveStyle(`
+        justify-self: start;
+        align-self: stretch;
+        max-height: 100vh;
+        `);
+  });
+});

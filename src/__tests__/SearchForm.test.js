9c791f1de31dac1f0c53abab1ffc00af958989e5
Vanilna <lesyalp@gmail.com>
2020-05-05 16:41:07 +0200

    searchForm - radio group change test

--- src/__tests__/SearchForm.test.js
+++ src/__tests__/SearchForm.test.js
@@ -70,6 +70,8 @@ describe("Search form component on start", () => {
     });
   });
 
+  it("shows animation while fetching manifest", () => {});
+
   it("it enables see latest button when rover is set, search button is still disabled", async () => {
     const { getByLabelText, getByText } = renderSearchForm();
     fireRoverChange(getByLabelText);
@@ -78,153 +80,24 @@ describe("Search form component on start", () => {
       expect(getByText(/search/i)).toBeDisabled();
     });
   });
+
+  it("should handle change sol/date radio group correctly", async () => {
+    const { getByLabelText } = renderSearchForm();
+    fireRoverChange(getByLabelText);
+
+    let sol;
+    let date;
+    await waitFor(() => {
+      sol = getByLabelText("SOL*");
+      date = getByLabelText(/earth date/i);
+    });
+    expect(sol).toBeChecked();
+    expect(date).not.toBeChecked();
+    fireEvent.click(date);
+    expect(sol).not.toBeChecked();
+    expect(date).toBeChecked();
+    fireEvent.click(sol);
+    expect(sol).toBeChecked();
+    expect(date).not.toBeChecked();
+  });
 });
-
-// it("redirects to results when search is fired", () => {});
-// it("renders search button", () => {
-//   const { getByText } = searchForm;
-//   const button = getByText(/search/i);
-//   expect(button).toBeInTheDocument();
-//   expect(getRoles(button).button).toBeTruthy();
-// });
-// it("renders 'see latest' button", () => {
-//   const { getByText } = searchForm;
-//   const button = getByText(/see latest/i);
-//   expect(button).toBeInTheDocument();
-//   expect(getRoles(button).button).toBeTruthy();
-// });
-
-// it("renders buttons disabled on start", () => {
-//   const { getByText } = searchForm;
-//   expect(getByText(/search/i)).toBeDisabled();
-//   expect(getByText(/see latest/i)).toBeDisabled();
-// });
-// it("enables see latest button when rover is picked", () => {
-//   const { getByText, getByLabelText } = searchForm;
-//   const select = getByLabelText(/choose rover/i);
-//   const button = getByText(/see latest/i);
-//   expect(button).toBeDisabled();
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-//   expect(button).toBeEnabled();
-// });
-// it("doesn't enables search button when only rover is picked", () => {
-//   const { getByText, getByLabelText } = searchForm;
-//   const select = getByLabelText(/choose rover/i);
-//   const button = getByText(/search/i);
-//   expect(button).toBeDisabled();
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-//   expect(button).toBeDisabled();
-// });
-// it("renders radio group component when rover is picked", () => {
-//   const { getAllByRole, getByLabelText, getAllByLabelText } = render(
-//     <SearchForm {...store} displayLeft={false} />
-//   );
-//   const select = getByLabelText(/choose rover/i);
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-//   const radio = getAllByRole(/radio/i);
-//   expect(radio.length).toBe(2);
-//   const solRadio = getAllByLabelText(/sol/i);
-//   const earthDayRadio = getByLabelText(/earth/i);
-//   expect(solRadio[0]).toBeInTheDocument();
-//   expect(earthDayRadio).toBeInTheDocument();
-// });
-// it("renders input element for sol/earth days when rover is picked", () => {
-//   const { getByLabelText, getByPlaceholderText } = render(
-//     <SearchForm {...store} displayLeft={false} />
-//   );
-//   const select = getByLabelText(/choose rover/i);
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-//   const selectDay = getByPlaceholderText(/SOL from 0 to/i);
-//   expect(selectDay).toBeInTheDocument();
-// });
-// it("changes date input element from sol to Earth days when radio group is changed", () => {
-//   const { getByLabelText, getByPlaceholderText, getAllByRole } = render(
-//     <SearchForm {...store} displayLeft={false} />
-//   );
-//   const select = getByLabelText(/choose rover/i);
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-//   const radioBtns = getAllByRole(/radio/i);
-//   fireEvent.click(radioBtns[1]);
-//   expect(radioBtns[0].checked).toBeFalsy();
-//   expect(radioBtns[1].checked).toBeTruthy();
-//   const input = getByPlaceholderText(/Date from/i);
-//   expect(input).toBeInTheDocument();
-// });
-// it("shows error when sol/day is incorrect", () => {
-//   const {
-//     getByLabelText,
-//     getByPlaceholderText,
-//     getAllByRole,
-//     getByText,
-//   } = render(<SearchForm {...store} displayLeft={false} />);
-//   const select = getByLabelText(/choose rover/i);
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-//   const radioBtns = getAllByRole(/radio/i);
-//   const inputSol = getByPlaceholderText(/SOL from 0 to/i);
-
-//   fireEvent.change(inputSol, { target: { value: "k" } });
-//   const errorSOL = getByText(/SOL should be a number from 0/i);
-//   expect(errorSOL).toBeInTheDocument();
-
-//   fireEvent.change(inputSol, { target: { value: "-1" } });
-//   expect(errorSOL).toBeInTheDocument();
-
-//   fireEvent.change(inputSol, { target: { value: "10000000" } });
-//   expect(errorSOL).toBeInTheDocument();
-
-//   fireEvent.click(radioBtns[1]);
-//   const inputDate = getByPlaceholderText(/Date from/i);
-
-//   fireEvent.change(inputDate, { target: { value: "20k1-02" } });
-//   const errorDate = getByText(/Date should be from/i);
-//   expect(errorDate).toBeInTheDocument();
-
-//   fireEvent.change(inputDate, { target: { value: "20,1-02-0." } });
-//   expect(errorDate).toBeInTheDocument();
-
-//   fireEvent.change(inputDate, { target: { value: "2012-02-006" } });
-//   expect(errorDate).toBeInTheDocument();
-
-//   fireEvent.change(inputDate, { target: { value: "1200-06-03" } });
-//   expect(errorDate).toBeInTheDocument();
-
-//   fireEvent.change(inputDate, { target: { value: "2050-06-03" } });
-//   expect(errorDate).toBeInTheDocument();
-// });
-// it("enables search button when form is filed", () => {
-//   const { getByLabelText, getByPlaceholderText, getByText } = render(
-//     <SearchForm {...store} displayLeft={false} />
-//   );
-//   const select = getByLabelText(/choose rover/i);
-//   fireEvent.change(select, { target: { value: "opportunity" } });
-
-//   const button = getByText(/search/i);
-//   expect(button).toBeDisabled();
-
-//   const inputSol = getByPlaceholderText(/SOL from 0 to/i);
-//   fireEvent.change(inputSol, { target: { value: "0" } });
-
-//   //wait for validation data from API
-//   setTimeout(() => expect(button).toBeEnabled(), 3000);
-
-//   fireEvent.change(inputSol, { target: { value: "k" } });
-
-//   expect(button).toBeDisabled();
-// });
-
-// it("calls search function", () => {
-//   //   const onSearch = jest.fn();
-//   //   const { getByLabelText, getByPlaceholderText, getByText } = render(
-//   //     <SearchForm {...store} displayLeft={false} handleSearch={onSearch} />
-//   //   );
-//   //   const select = getByLabelText(/choose rover/i);
-//   //   fireEvent.change(select, { target: { value: "opportunity" } });
-//   //   const inputSol = getByPlaceholderText(/SOL from 0 to/i);
-//   //   fireEvent.change(inputSol, { target: { value: "0" } });
-//   //   const button = getByText(/search/i);
-//   //   //wait for validation data from API
-//   //   setTimeout(() => {
-//   //     fireEvent.click(button);
-//   //     expect(onSearch).toBeCalledTimes(1);
-//   //   }, 3000);
-// });

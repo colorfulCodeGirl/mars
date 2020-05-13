da8b106aa8f721edc6ab10a62e1bff9ca35e3671
Vanilna <lesyalp@gmail.com>
2020-05-13 13:15:12 +0200

    SearchModal tests for closing form on new serch - failing tests

--- src/__tests__/SearchModal.test.js
+++ src/__tests__/SearchModal.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "../store/store";
 import SearchModal from "../components/molecules/SearchModal";
@@ -18,6 +18,26 @@ jest.mock("react-router-dom", () => ({
   useHistory: jest.fn().mockReturnValue([]),
 }));
 
+const pickRover = (getByLabelText) => {
+  const mockData = {
+    photo_manifest: {
+      landing_date: "2004-01-04",
+      max_sol: 2208,
+      max_date: "2010-03-21",
+    },
+  };
+
+  global.fetch = jest
+    .fn()
+    .mockImplementation(() =>
+      Promise.resolve({ ok: true, json: () => mockData })
+    );
+
+  fireEvent.change(getByLabelText(/choose rover/i), {
+    target: { value: "Curiosity" },
+  });
+};
+
 describe("SearchModal", () => {
   it("should render button, but not modal on start", () => {
     const { getByText, queryByTestId } = renderSearchModal();
@@ -30,6 +50,37 @@ describe("SearchModal", () => {
     expect(queryByTestId(/form/i)).toBeInTheDocument();
 
     fireEvent.click(getByLabelText(/close/i));
+    expect(queryByTestId(/form/i)).not.toBeInTheDocument();
+  });
+  it("should close modal on new search (see latest)", async () => {
+    const { getByText, queryByTestId, getByLabelText } = renderSearchModal();
+    fireEvent.click(getByText(/change search/i));
+    expect(queryByTestId(/form/i)).toBeInTheDocument();
+
+    pickRover(getByLabelText);
+
+    await waitFor(() => {
+      const seeLatestBtn = getByText(/see latest/i);
+      expect(seeLatestBtn).toBeEnabled();
+      fireEvent.click(seeLatestBtn);
+    });
+    expect(queryByTestId(/form/i)).not.toBeInTheDocument();
+  });
+  it("should close modal on new search (search)", async () => {
+    const { getByText, queryByTestId, getByLabelText } = renderSearchModal();
+    fireEvent.click(getByText(/change search/i));
+    expect(queryByTestId(/form/i)).toBeInTheDocument();
+
+    pickRover(getByLabelText);
+
+    await waitFor(() => {
+      const solInput = getByLabelText(/sol from 0/i);
+      expect(solInput).toBeInTheDocument();
+      fireEvent.change(solInput, { target: { value: "54" } });
+    });
+    const searchBtn = getByText(/search/i);
+    expect(searchBtn).toBeEnabled();
+    fireEvent.click(searchBtn);
     expect(queryByTestId(/form/i)).not.toBeInTheDocument();
   });
 });

6eb2249a7d0cb592fb3a70373285a39ec2a9dc93
Vanilna <lesyalp@gmail.com>
2020-05-14 12:32:18 +0200

    refactored fetchData function to catch errors

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,10 @@
 export const fetchData = async (urlParams) => {
   const apiKey = process.env.REACT_APP_API_CODE;
-  const response = await fetch(
+  const data = await fetch(
     `https://api.nasa.gov/mars-photos/api/v1/${urlParams}&api_key=${apiKey}`
-  );
-  const data = await response.json();
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
   return data;
 };
 

3e92c75737d9a77de8497cb75f3eb29cf4a03f87
Vanilna <lesyalp@gmail.com>
2020-05-14 13:04:57 +0200

    block Animated Mars from showing in Storybook

--- src/stories/AnimatedMars.stories.js
+++ src/stories/AnimatedMars.stories.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { addDecorator } from "@storybook/react";
-import AnimatedMars from "../components/atoms/AnimatedMars";
-import { GlobalStyles } from "../App";
+// import React, { useState, useEffect } from "react";
+// import { addDecorator } from "@storybook/react";
+// import AnimatedMars from "../components/atoms/AnimatedMars";
+// import { GlobalStyles } from "../App";
 
-addDecorator((s) => (
-  <>
-    <GlobalStyles />
-    {s()}
-  </>
-));
+// addDecorator((s) => (
+//   <>
+//     <GlobalStyles />
+//     {s()}
+//   </>
+// ));
 
-export default {
-  title: "AnimatedMars",
-  component: AnimatedMars,
-};
+// export default {
+//   title: "AnimatedMars",
+//   component: AnimatedMars,
+// };
 
-export const Regular = () => {
-  const [isAnimating, setIsAnimating] = useState(true);
+// export const Regular = () => {
+//   const [isAnimating, setIsAnimating] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => setIsAnimating(false), 7000);
-  }, []);
+//   useEffect(() => {
+//     setTimeout(() => setIsAnimating(false), 7000);
+//   }, []);
 
-  return <AnimatedMars isAnimating={isAnimating} />;
-};
+//   return <AnimatedMars isAnimating={isAnimating} />;
+// };

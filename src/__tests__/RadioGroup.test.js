783e8f31fbc8bb07fa9ec1510583d2526c3c5158
Vanilna <lesyalp@gmail.com>
2020-05-15 08:23:23 +0200

    refactor radioGroup tests - create renderRadioGroup function

--- src/__tests__/RadioGroup.test.js
+++ src/__tests__/RadioGroup.test.js
@@ -2,42 +2,33 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import RadioGroup from "../components/atoms/RadioGroup";
 
+const renderRadioGroup = (checkedIndex = 0, changeHandler = () => {}) =>
+  render(
+    <RadioGroup
+      options={["one", "two"]}
+      category="sol"
+      changeHandler={changeHandler}
+      checkedIndex={checkedIndex}
+    />
+  );
+
 describe("Radio group component", () => {
   it("to render properly", () => {
-    const { getByLabelText } = render(
-      <RadioGroup
-        options={["one", "two"]}
-        category="sol"
-        changeHandler={() => {}}
-      />
-    );
+    const { getByLabelText } = renderRadioGroup();
 
     expect(getByLabelText(/one/i)).toBeInTheDocument();
     expect(getByLabelText(/two/i)).toBeInTheDocument();
   });
 
   it("to have first option checked by default", () => {
-    const { getByLabelText } = render(
-      <RadioGroup
-        options={["one", "two"]}
-        category="sol"
-        changeHandler={() => {}}
-      />
-    );
+    const { getByLabelText } = renderRadioGroup();
 
     expect(getByLabelText(/one/i)).toBeChecked();
     expect(getByLabelText(/two/i)).not.toBeChecked();
   });
 
   it("to have other option checked", () => {
-    const { getByLabelText } = render(
-      <RadioGroup
-        options={["one", "two"]}
-        category="sol"
-        changeHandler={() => {}}
-        checkedIndex={1}
-      />
-    );
+    const { getByLabelText } = renderRadioGroup(1);
 
     expect(getByLabelText(/one/i)).not.toBeChecked();
     expect(getByLabelText(/two/i)).toBeChecked();
@@ -45,13 +36,7 @@ describe("Radio group component", () => {
 
   it("should fire click event", () => {
     const clickHandler = jest.fn();
-    const { getByLabelText } = render(
-      <RadioGroup
-        options={["one", "two"]}
-        category="sol"
-        changeHandler={clickHandler}
-      />
-    );
+    const { getByLabelText } = renderRadioGroup(0, clickHandler);
     fireEvent.click(getByLabelText(/two/i));
     expect(clickHandler).toBeCalledTimes(1);
   });

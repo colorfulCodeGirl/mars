7024a844aeda8cc6f9f022eb304166e3a45674c5
Vanilna <lesyalp@gmail.com>
2020-05-15 08:42:33 +0200

    refactor Select test - create renderSelect function

--- src/__tests__/Select.test.js
+++ src/__tests__/Select.test.js
@@ -2,42 +2,31 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Select from "../components/atoms/Select";
 
+const renderSelect = (changeHandler = () => {}) =>
+  render(
+    <Select
+      options={["one", "two"]}
+      changeHandler={changeHandler}
+      defaultValue="Choose rover"
+      name="rover"
+    />
+  );
+
 describe("Select element", () => {
   it("renders select input with label", () => {
-    const { getByLabelText } = render(
-      <Select
-        options={["one", "two"]}
-        changeHandler={() => {}}
-        defaultValue="Choose rover"
-        name="rover"
-      />
-    );
-
+    const { getByLabelText } = renderSelect();
     expect(getByLabelText(/choose rover/i)).toBeInTheDocument();
   });
-  it("hides default option", () => {
-    const { getAllByText } = render(
-      <Select
-        options={["one", "two"]}
-        changeHandler={() => {}}
-        defaultValue="Choose rover"
-        name="rover"
-      />
-    );
 
+  it("hides default option", () => {
+    const { getAllByText } = renderSelect();
     const elements = getAllByText(/choose/i);
     expect(elements[1]).not.toBeVisible();
   });
+
   it("handles change event", () => {
     const onChange = jest.fn();
-    const { getByLabelText } = render(
-      <Select
-        options={["one", "two"]}
-        changeHandler={onChange}
-        defaultValue="Choose rover"
-        name="rover"
-      />
-    );
+    const { getByLabelText } = renderSelect(onChange);
     const select = getByLabelText(/choose rover/i);
     fireEvent.change(select, { target: { value: "two" } });
     expect(onChange).toHaveBeenCalledTimes(1);
